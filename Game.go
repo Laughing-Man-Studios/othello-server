16e72ca13d6628c4d5702f6487872b42a8922a2b
Robert Gibb <rgibb@rethinkrobotics.com>
2017-05-16 23:41:02 -0400

    Added functionality for generating possible moves and validating moves

--- Game.go
+++ Game.go
@@ -2,32 +2,91 @@ package main
 
 import "fmt"
 
-//Game is a struct that represents the status of the game
-type Game struct {
+type game struct {
 	board [8][8]int
 	score map[int]int
 }
 
-var game = &Game{
+var theGame = game{
 	[8][8]int{},
 	map[int]int{},
 }
 
-//SetupGame sets the board and score up for a new game
-func SetupGame() {
-	game.board[3][3] = 1
-	game.board[4][4] = 1
-	game.board[3][4] = 2
-	game.board[4][3] = 2
-	game.score[1] = 2
-	game.score[2] = 2
-	PrintGame()
+func setupGame() {
+	theGame.board[3][3] = 1
+	theGame.board[4][4] = 1
+	theGame.board[3][4] = 2
+	theGame.board[4][3] = 2
+	theGame.score[1] = 2
+	theGame.score[2] = 2
+	printGame(&theGame.board)
+	findPotentialMoves(theGame.board, 1)
 }
 
-//PrintGame prints out the board and score of the game
-func PrintGame() {
-	for _, row := range game.board {
+func findPotentialMoves(board [8][8]int, p int) {
+	for rowIndex, row := range board {
+		for colIndex := range row {
+			if theGame.board[rowIndex][colIndex] == p {
+				checkDirection(0, 1, rowIndex, colIndex, p, &board)
+				checkDirection(1, 1, rowIndex, colIndex, p, &board)
+				checkDirection(0, -1, rowIndex, colIndex, p, &board)
+				checkDirection(1, 0, rowIndex, colIndex, p, &board)
+				checkDirection(-1, -1, rowIndex, colIndex, p, &board)
+				checkDirection(-1, 0, rowIndex, colIndex, p, &board)
+				checkDirection(-1, 1, rowIndex, colIndex, p, &board)
+			}
+		}
+	}
+	printGame(&board)
+}
+
+func checkDirection(offsetY int, offsetX int, originX int, originY int, p int, board *[8][8]int) {
+	previousTile := board[originX][originY]
+	tile := board[originX+offsetX][originY+offsetY]
+
+	if tile != p && tile != 0 && tile != 3 {
+		checkDirection(offsetY, offsetX, originX+offsetX, originY+offsetY, p, board)
+	} else if previousTile != p && previousTile != 0 && previousTile != 3 && tile == 0 {
+		board[originX+offsetX][originY+offsetY] = 3
+	}
+}
+
+func movePiece(move moveData) bool {
+	var valid = false
+
+	for i := -1; i < 2; i++ {
+		for j := -1; i < 2; i++ {
+			if i != 0 && j != 0 {
+				moveMaid := validateCheckDirection(i, j, move.Row, move.Col, move.Player)
+				if moveMaid == true {
+					valid = true
+				}
+			}
+		}
+	}
+
+	return valid
+}
+
+func validateCheckDirection(offsetY int, offsetX int, originX int, originY int, p int) bool {
+	previousTile := theGame.board[originX][originY]
+	tile := theGame.board[originX+offsetX][originY+offsetY]
+
+	if tile != p && tile != 0 && tile != 3 {
+		if validateCheckDirection(offsetY, offsetX, originX+offsetX, originY+offsetY, p) == true {
+			theGame.board[originX][originY] = p
+			return true
+		}
+	} else if previousTile != p && previousTile != 0 && previousTile != 3 && tile == p {
+		return true
+	}
+
+	return false
+}
+
+func printGame(board *[8][8]int) {
+	for _, row := range board {
 		fmt.Println(row)
 	}
-	fmt.Printf("Player1: %v - Player2: %v\n", game.score[1], game.score[2])
+	fmt.Printf("Player1: %v - Player2: %v\n", theGame.score[1], theGame.score[2])
 }
--- Handler.go
+++ Handler.go
@@ -11,27 +11,24 @@ import (
 	"github.com/gorilla/schema"
 )
 
-//MoveData tells where a peice moved from and to, and the player who made the move
-type MoveData struct {
+type moveData struct {
 	Row    int
 	Col    int
 	Player int
 }
 
-//NewGame is a handler for starting a new Game of Othello
-func NewGame(w http.ResponseWriter, r *http.Request) {
+func newGame(w http.ResponseWriter, r *http.Request) {
 	numOfPlayer := len(b.subscribers)
 	if numOfPlayer > 1 {
 		fmt.Fprint(w, 0)
 	} else {
 		fmt.Fprint(w, numOfPlayer+1)
 	}
-	SetupGame()
+	setupGame()
 }
 
-//Move is a handler for logging a move made by a player
-func Move(w http.ResponseWriter, r *http.Request) {
-	var move MoveData
+func move(w http.ResponseWriter, r *http.Request) {
+	var move moveData
 	vars := mux.Vars(r)
 	player, err := strconv.Atoi(vars["player"])
 	if err != nil {
@@ -47,20 +44,25 @@ func Move(w http.ResponseWriter, r *http.Request) {
 		decoder := schema.NewDecoder()
 		decoder.Decode(&move, r.PostForm)
 
-		moveJSON, err := json.Marshal(move)
-		if err != nil {
-			log.Fatal(err)
+		validMove := movePiece(move)
+		if !validMove {
+			fmt.Fprintf(w, "Player %v: Invalid Move", move.Player)
+		} else {
+			moveJSON, err := json.Marshal(move)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			jsonStr := string(moveJSON)
+			Publish(jsonStr)
+
+			fmt.Fprintf(w, "Player %v: Move Initiated", move.Player)
 		}
 
-		jsonStr := string(moveJSON)
-		Publish(jsonStr)
-
-		fmt.Fprintf(w, "Player %v: Move Initiated", move.Player)
 	}
 }
 
-//Events is the handler for connecting with the SSE broker
-func Events(w http.ResponseWriter, r *http.Request) {
+func events(w http.ResponseWriter, r *http.Request) {
 	f := w.(http.Flusher)
 	ch := Subscribe()
 	defer Unsubscribe(ch)
